416e4f36dbe204939eb2a636ed087957ba9a4129
Salvador Sierra <salvilopezronda@gmail.com>
2021-04-05 20:13:33 +0200

    commit firs revision

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule } from '@angular/forms';
-// Angular Material modules
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './views/list/list.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactDetailPageComponent } from './pages/contact-detail-page/contact-detail-page.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TodoComponent } from './components/todo/todo/todo.component';
 import { TodoDetailPageComponent } from './pages/todo-detail-page/todo-detail-page.component';
 import { ContactListPageComponent } from './pages/contact-list-page/contact-list-page.component';
 import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.component';
+import { ContactListComponent } from './views/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
     ContactComponent,
     HomePageComponent,
     LoginPageComponent,
@@ -41,7 +42,8 @@ import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.com
     TodoComponent,
     TodoDetailPageComponent,
     ContactListPageComponent,
-    TodoListPageComponent
+    TodoListPageComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
@@ -60,6 +62,9 @@ import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.com
     NgbModule,
     FormsModule,
     DragDropModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
 
   ],
   providers: [],
